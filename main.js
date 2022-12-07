963d6549cdf4631fd2e5de508a86289e62bf6f4b
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-07 16:47:03 +0100

    Add more element

--- main.js
+++ main.js
@@ -61,83 +61,86 @@ const containerElement = document.getElementById("container");
 
 posts.forEach(image => {
     //CREO il postElement, AGGIUNGO la classe post, INSERISCO il postElement nel containerElement
-const postElement = document.createElement("div");
-postElement.classList.add("post");
-containerElement.append(postElement);
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    containerElement.append(postElement);
 
 
-//CREO l'headerPostElement, AGGIUNGO la classe post_header, INSERISCO headerPostElement nel postElement
-const headerPostElement = document.createElement("div");
-headerPostElement.classList.add("post__header");
-postElement.append(headerPostElement);
+    //CREO l'headerPostElement, AGGIUNGO la classe post_header, INSERISCO headerPostElement nel postElement
+    const headerPostElement = document.createElement("div");
+    headerPostElement.classList.add("post__header");
+    postElement.append(headerPostElement);
 
-//CREO l'postMetaElement, AGGIUNGO la classe post-meta, INSERISCO il postMetaElement nel headerPostElement
-const postMetaElement = document.createElement("div");
-postMetaElement.classList.add("post-meta");
-headerPostElement.append(postMetaElement);
+    //CREO l'postMetaElement, AGGIUNGO la classe post-meta, INSERISCO il postMetaElement nel headerPostElement
+    const postMetaElement = document.createElement("div");
+    postMetaElement.classList.add("post-meta");
+    headerPostElement.append(postMetaElement);
 
-//CREO il postMetaIconElement, AGGIUNGO la classe post-meta__icon, INSERISCO il postMetaElement nel postMetaElement
-const postMetaIconElement = document.createElement("div");
-postMetaIconElement.classList.add("post-meta__icon");
-postMetaElement.append(postMetaIconElement);
+    //CREO il postMetaIconElement, AGGIUNGO la classe post-meta__icon, INSERISCO il postMetaElement nel postMetaElement
+    const postMetaIconElement = document.createElement("div");
+    postMetaIconElement.classList.add("post-meta__icon");
+    postMetaElement.append(postMetaIconElement);
 
-//CREO l'imageElement, AGGIUNGO la classe profile-pic, MODIFICO le proprietà src e alt, INSERISCO l'imageElement in postMetaIconElement
-const imageElement = document.createElement("img");
-imageElement.classList.add("profile-pic");
-imageElement.src= image.author.image;
-imageElement.alt= image.author.name;
-postMetaIconElement.append(imageElement);
+    //CREO l'imageElement, AGGIUNGO la classe profile-pic, MODIFICO le proprietà src e alt, INSERISCO l'imageElement in postMetaIconElement
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("profile-pic");
+    imageElement.src = image.author.image;
+    imageElement.alt = image.author.name;
+    postMetaIconElement.append(imageElement);
 
-//          META DATA ELEMENT -- > SALVIAMO LE INFORMAZIONI DELL'AUTORE E DELLA DATA DI CREAZIONE DEL POST
-//CREO il metaDataELement, AGGIUNGO la classe post-meta__data, INSERISCO nel postMetaElement
-const metaDataELement = document.createElement("div");
-metaDataELement.classList.add("post-meta__data");
-postMetaElement.append(metaDataELement);
+    //          META DATA ELEMENT -- > SALVIAMO LE INFORMAZIONI DELL'AUTORE E DELLA DATA DI CREAZIONE DEL POST
+    //CREO il metaDataELement, AGGIUNGO la classe post-meta__data, INSERISCO nel postMetaElement
+    const metaDataELement = document.createElement("div");
+    metaDataELement.classList.add("post-meta__data");
+    postMetaElement.append(metaDataELement);
 
-//CREO il authorElement, AGGIUNGO la classe post-meta__author, MODIFICO l'innerHTML, INSERISCO il authorElement nel metaDataELement
-const authorElement = document.createElement("div");
-authorElement.classList.add("post-meta__author");
-authorElement.innerHTML = image.author.name;
-metaDataELement.append(authorElement);
+    //CREO il authorElement, AGGIUNGO la classe post-meta__author, MODIFICO l'innerHTML, INSERISCO il authorElement nel metaDataELement
+    const authorElement = document.createElement("div");
+    authorElement.classList.add("post-meta__author");
+    authorElement.innerHTML = image.author.name;
+    metaDataELement.append(authorElement);
 
-//CREO il authorElement, AGGIUNGO la classe post-meta__author, MODIFICO l'innerHTML, INSERISCO il authorElement nel postMetaElement
-const timeElement = document.createElement("div");
-timeElement.classList.add("post-meta__time");
-timeElement.innerHTML = image.created;
-metaDataELement.append(timeElement);
+    //CREO il authorElement, AGGIUNGO la classe post-meta__author, MODIFICO l'innerHTML, INSERISCO il authorElement nel postMetaElement
+    const timeElement = document.createElement("div");
+    timeElement.classList.add("post-meta__time");
+    timeElement.innerHTML = image.created;
+    metaDataELement.append(timeElement);
 
 
+    // PARTE DEL CONTENUTO VA APPESO NEL post content
+    //CREO il conenuto del post, INSERISCO nel postElement
+    const postContent = document.createElement("div");
+    postContent.classList.add("post__text");
+    postContent.innerHTML = image.content;
+    postElement.append(postContent);
 
-// PARTE DEL CONTENUTO VA APPESO NEL post content
-//CREO il conenuto del post
-const postContent = document.createElement("div");
-postContent.classList.add("post__text");
-postContent.innerHTML = image.content;
-postElement.append(postContent);
+    //PARTE DELL'IMMAGINE, va inserita nel postElement
+    //CREO il postDivImage (il parent della postImage), AGGIUGNO le classi, INSERISCO nel postElement
+    const postDivImage = document.createElement("div");
+    postDivImage.classList.add("post__image");
+    postElement.append(postDivImage);
+
+    //CREO il postImage, MODIFICO le proprietà src e alt, INSERISCO nel postDivImage
+    const postImage = document.createElement("img");
+    postImage.src = image.media;
+    postImage.alt = image.author.name;
+    postDivImage.append(postImage);
+
+
+    //PARTE DEL POST FOOTER CON BOTTONE PER IL "MI PIACE" E NUMERO DI MI PIACE, APPENDERE NEL postElement
+    //CREO il postFooterElement (parent), INSERISCO nel postElement
+    const postFooterElement = document.createElement("div");
+    postFooterElement.classList.add("post__footer");
+    postElement.append(postFooterElement);
 });
 
 
-// post di esempio/template, da togliere/commentare e generare da JS
-//         <div class="post"> ok
-//             <div class="post__header"> ok 
-//                 <div class="post-meta">  
-      ok            
-//                     <div class="post-meta__icon"> ok 
-//                         <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">  ok                  
-//                     </div> fine div image
-
-//                     <div class="post-meta__data">
-//                         <div class="post-meta__author">Phil Mangione</div>
-//                         <div class="post-meta__time">4 mesi fa</div>
-//                     </div>
-
-//                 </div>
-//             </div>
 
 //             <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
 //             <div class="post__image">
 //                 <img src="https://unsplash.it/600/300?image=171" alt="">
 //             </div>
+
 //             <div class="post__footer">
 //                 <div class="likes js-likes">
 //                     <div class="likes__cta">
@@ -149,8 +152,8 @@ postElement.append(postContent);
 //                     <div class="likes__counter">
 //                         Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
 //                     </div>
-//                 </div> 
-//             </div>            
+//                 </div>
+//             </div>
 //         </div>
 
 
